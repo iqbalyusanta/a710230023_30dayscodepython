44fdfddc8c37cfd9f8ae91a926673ec8dc00fefd
iqbalyusanta <a710230023@student.ums.ac.id>
2024-06-07 09:19:18 +0700

    Add files via upload

---
+++ hari ke 4.PY
@@ -0,0 +1,27 @@
+class Kendaraan:
+    def __init__(self, jenis):
+        self.jenis = jenis
+
+class Sepeda(Kendaraan):
+    def __init__(self, jenis, warna):
+        super().__init__(jenis)
+        self.warna = warna
+
+class SepedaMotor(Sepeda):
+    def __init__(self, jenis, warna, merk):
+        super().__init__(jenis, warna)
+        self.merk = merk
+
+class Mobil(SepedaMotor):
+    def __init__(self, jenis, warna, merk, tahun):
+        super().__init__(jenis, warna, merk)
+        self.tahun = tahun
+
+    def info(self):
+        print(f"Jenis: {self.jenis}")
+        print(f"Warna: {self.warna}")
+        print(f"Merk: {self.merk}")
+        print(f"Tahun: {self.tahun}")
+
+mobil1 = Mobil("Sedan", "Merah", "Toyota", 2022)
+mobil1.info()
