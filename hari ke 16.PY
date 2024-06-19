b3c9fc507a292099e3ef1cefad053e52648a216a
iqbalyusanta <a710230023@student.ums.ac.id>
2024-06-19 12:58:49 +0700

    Add files via upload

---
+++ hari ke 16.PY
@@ -0,0 +1,12 @@
+def divide_numbers(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
+    else:
+        return a/b
+
+try:
+    result = divide_numbers(10, 0)
+except ValueError as e:
+    print(e)
+else:
+    print("The result is:", result)
